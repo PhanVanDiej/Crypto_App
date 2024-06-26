907ca4f7760cced23a19f9d45852d9b89bdf0f81
YA DAT <22520243@gm.uit.edu.vn>
2024-06-26 21:22:09 +0700

    yadat

---
+++ appchinhthuc/app/src/main/java/com/example/appchinhthuc/Activity/DetailCryptoActivity.kt
@@ -0,0 +1,17 @@
+package com.example.appchinhthuc.Activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.appchinhthuc.R
+
+class DetailCryptoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_detail_crypto)
+
+    }
+}
